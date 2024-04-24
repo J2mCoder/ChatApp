4fa931294ae5f4701c4bb9ae3be93ccdd6a2268d
J2mCoder <jeandedieumbumba2802@gmail.com>
2024-04-24 02:05:09 +0100

    finish block right

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form")
 
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const inputValue = form.elements["promt"].value;
+    console.log(inputValue);
+  })
+
+})
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = ""
+  loadInterval = setInterval(() => {
+    element.textContent += "."
+    if (element.textContent === "....") {
+      element.textContent = ""
+    }
+  }, 300)
+}
+
+function typeText(element, text) {
+  let index = 0
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index)
+      index++
+    } else {
+      clearInterval(interval)
+    }
+  }, 50)
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+  return `id-${timestamp}-${hexadecimalString}`
+}
+
+console.log(generateUniqueId())
